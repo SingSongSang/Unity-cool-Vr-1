db25031c98bb8efb5ffddc6934215062bf39c16c
Sang Nguyen <111586770+SingSongSang@users.noreply.github.com>
2022-10-14 10:35:39 -0600

    cool stuff

---
+++ 2.1 Unity/2.4 Challange/Assets/Sunnyland/Script/Player.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Vector3 playerPosition = new Vector3(0,0,0);
+
+    string Starting = "player is very smelly";
+
+    Rigidbody2D playerPhyiscs = null;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(Starting);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxisRaw("Horizontal"));
+    }
+}

7f4436df56d1b592670edd856976a3809768b605
Sang Nguyen <111586770+SingSongSang@users.noreply.github.com>
2022-11-08 09:58:43 -0700

    2.7

--- 2.1 Unity/2.6 Challange/Assets/Scripts/PlayerMovement.cs
+++ 2.1 Unity/2.6 Challange/Assets/Scripts/PlayerMovement.cs
@@ -5,27 +5,34 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     //speed modifier
+    public Animator animator;
     public float speed = 2;
     private Rigidbody2D rb;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         rb.velocity = (new Vector3((Input.GetAxisRaw("Horizontal")) * speed, (Input.GetAxisRaw("Vertical")) * speed, 0));
-        Vector2 flip = new Vector3(-1, 1 , 1);
-        transform.localScale *= flip;
-        /*if (Input.GetAxisRaw("Horizontal") < 0)
-         {
-             transform.localScale = new Vector3(-5, 5, 0);
-         }
-         if (Input.GetAxisRaw("Horizontal")> 0)
-         {
-             transform.localScale = new Vector3(5, 5, 0);
-         }*/
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            animator.SetBool("Moving", true);
+            transform.localScale = new Vector3(-5, 5, 0);
+        }
+        else if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            animator.SetBool("Moving", true);
+            transform.localScale = new Vector3(5, 5, 0);
+        }
+        else
+        {
+            animator.SetBool("Moving", false);
+        }
+
     }
 
 }
---
+++ 2.1 Unity/2.7 Challenge/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public Animator anim;
+    public float speed = 2;
+    public float jump = 300;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); 
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //getting the player input and moving
+        transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal")*Time.deltaTime * speed,0, 0));
+
+        //flipping and animating horizontal movement
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            transform.localScale = (new Vector3(5, 5, 0));
+            anim.SetBool("Moving", true);
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            transform.localScale = (new Vector3(-5, 5, 0));
+            anim.SetBool("Moving", true);
+        }
+
+        //if you dont press move dont use moving animation
+        else
+        {
+            anim.SetBool("Moving", false);
+        }
+
+        //Jumping Movement
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb.AddForce(transform.up * jump);
+        }
+
+        //animation for jumping, also turns off walking animation 
+        if (Input.GetKey(KeyCode.W))
+        {
+            anim.SetBool("Jumping", true);
+            anim.SetBool("Moving", false);
+        }
+        else
+        {
+            anim.SetBool("Jumping", false);
+        }
+    }
+}
